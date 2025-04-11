5be7c30b77ad0daa53bf5df059acea5915536000
Fidel-Arias <fr06arias16@gmail.com>
2025-04-10 19:04:38 -0500

    Fix(isPrime): Numeros cuadrados perfectos son identificados como primos

--- Primos/test/num_primos_test.py
+++ Primos/test/num_primos_test.py
@@ -1,8 +1,9 @@
 import pytest
 from ..num_primos import isPrime
 
-@pytest.mark.parametrize("n", {
-    (1),
-})
-def test_isPrime(n):
-    assert isPrime(n) == True
+
+def test_isPrime():
+    assert isPrime(1) == False
+
+def test_isPrime_fails_for_squares_of_primes():
+    assert isPrime(9) == False  # Esta prueba fallará con el código original
