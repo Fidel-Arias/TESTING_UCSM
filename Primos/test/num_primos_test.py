e16281dca381b94d07bca72f34c2c42073a961e1
Fidel-Arias <fr06arias16@gmail.com>
2025-04-10 19:24:53 -0500

    Test: Creación de tests con números cuadrados perfectos

--- Primos/test/num_primos_test.py
+++ Primos/test/num_primos_test.py
@@ -1,9 +1,17 @@
 import pytest
-from ..num_primos import isPrime
+from ..num_primos import isPrime, isPrime2
 
 
 def test_isPrime():
     assert isPrime(1) == False
 
+def test_isPrime_2():
+    assert isPrime2(4) == False
+    assert isPrime2(16) == False
+    assert isPrime2(25) == False
+    assert isPrime2(36) == False
+    assert isPrime2(49) == False
+    assert isPrime2(64) == False
+
 def test_isPrime_fails_for_squares_of_primes():
-    assert isPrime(9) == False  # Esta prueba fallará con el código original
+    assert isPrime2(9) == False  # Esta prueba fallará con el código original
