08307735565ddb996a5bca19b668a309f7ff1ed6
Fidel-Arias <fr06arias16@gmail.com>
2025-04-10 20:17:38 -0500

    Create: Creación de la carpeta Suma_bits

---
+++ Suma_bits/test/__init__.py
@@ -0,0 +1,1 @@
+from ..suma_bits import *
---
+++ Suma_bits/test/suma_bits_test.py
@@ -0,0 +1,9 @@
+
+
+def test():
+    # Write test cases that achieve 100%
+    # Statement coverage of add8. You
+    # will need to call add8 multiple
+    # times in order to do this.
+    ###Your code here.
+    pass
