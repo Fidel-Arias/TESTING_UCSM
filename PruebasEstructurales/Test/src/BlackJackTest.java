1652c12c3ea8dd90865116f229d148e1884a26a7
Fidel-Arias <fr06arias16@gmail.com>
2025-04-17 11:40:52 -0500

    Create: Creación de Pruebas Estructurales con el ejemplo de BlackJack

---
+++ PruebasEstructurales/Test/src/BlackJackTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BlackJackTest {
+    @Test
+    public void bothPlayersGoTooHigh() {
+        int result = new BlackJack().play(30, 30);
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void LeftPlayerWins() {
+        int result = new BlackJack().play(10, 9);
+        Assertions.assertEquals(10, result);
+    }
+}
